4146f1dc1fe605458dfcac5ba3fc1280b7c00f45
badido18 <hm_boudis@esi.dz>
2022-03-10 04:55:26 +0100

    v0.1

---
+++ index.js
@@ -0,0 +1,162 @@
+let matrix2DInit5 = [
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0]
+]
+
+
+class Matrix{
+    constructor(){
+        this.dimen = 5
+        this.state = matrix2DInit5
+        this.fillRandomTiles();
+    }
+
+    fillRandomTiles(){
+        // here there will be filling first step
+        // testing
+        this.state = [
+            [2,0,0,0,0],
+            [2,8,16,0,0],
+            [4,0,0,0,0],
+            [0,8,16,0,0],
+            [2,8,16,16,0]
+        ]
+    }
+
+    shiftUpY(y){
+        let shift = 0
+        let x=0
+        let prec = -1
+        while(x<this.dimen){
+            if(this.state[x][y] == 0){
+                x++
+            }
+            else{
+                if(prec == this.state[x][y]){
+                    this.state[shift-1][y] *=2
+                    shift--
+                    prec = -1
+                }else{
+                    this.state[shift][y] = this.state[x][y]
+                    prec = this.state[shift][y]
+                }
+                if(shift !=x)
+                    this.state[x][y] = 0
+                shift++
+                x=shift
+            }
+        }
+    }
+
+    shiftDownY(y){
+        let shift = this.dimen-1
+        let x= this.dimen-1
+        let prec = -1
+        while(x>-1){
+            if(this.state[x][y] == 0){
+                x--
+            }
+            else{
+                if(prec == this.state[x][y]){
+                    this.state[shift+1][y] *=2
+                    shift++
+                    prec = -1
+                }else{
+                    this.state[shift][y] = this.state[x][y]
+                    prec = this.state[shift][y]
+                }
+                if(shift !=x)
+                    this.state[x][y] = 0
+                shift--
+                x=shift
+            }
+        }
+    }
+
+    shiftRightX(x){
+        let shift = this.dimen-1
+        let y= this.dimen-1
+        let prec = -1
+        while(y>-1){
+            if(this.state[x][y] == 0){
+                y--
+            }
+            else{
+                if(prec == this.state[x][y]){
+                    this.state[x][shift+1] *=2
+                    shift++
+                    prec = -1
+                }else{
+                    this.state[x][shift]= this.state[x][y]
+                    prec = this.state[x][shift]
+                }
+                if(shift !=y)
+                    this.state[x][y] = 0
+                shift--
+                y=shift
+            }
+        }
+    }
+
+    shiftLeftX(x){
+        let shift = 0
+        let y = 0
+        let prec = -1
+        while(y<this.dimen){
+            if(this.state[x][y] == 0){
+                y++
+            }
+            else{
+                if(prec == this.state[x][y]){
+                    this.state[x][shift-1] *=2
+                    shift--
+                    prec = -1
+                }else{
+                    this.state[x][shift]= this.state[x][y]
+                    prec = this.state[x][shift]
+                }
+                if(shift !=y)
+                    this.state[x][y] = 0
+                shift++
+                y=shift
+            }
+        }
+    }
+
+    moveUp(){
+        for(let y=0;y<this.dimen;y++){
+            this.shiftUpY(y)
+        }
+        // fillRandomTiles()
+    }
+
+    moveDown(){
+        for(let y=0;y<this.dimen;y++){
+            this.shiftDownY(y)
+        }
+        // fillRandomTiles()
+    }
+
+    moveLeft(){
+        for(let x=0;x<this.dimen;x++){
+            this.shiftLeftX(x)
+        }
+        // fillRandomTiles()
+    }
+
+    moveRight(){
+        for(let x=0;x<this.dimen;x++){
+            this.shiftRightX(x)
+        }
+        // fillRandomTiles()
+    }
+}
+
+var test = new Matrix()
+
+console.log(test.state)
+test.moveLeft()
+console.log(test.state)
